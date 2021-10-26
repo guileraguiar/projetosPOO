99319b9b213d0689b759deee0be1e011467e5417
guileraguiar <54964014+guileraguiar@users.noreply.github.com>
2021-10-25 23:24:19 -0300

    Edição

--- jdbc/src/connection.java
+++ jdbc/src/connection.java
@@ -3,13 +3,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-class Main {
+public class Main {
 
     public static void main(String[] args) {
 
         try {
 
-            String url = "jdbc:sqlite:C:/Users/mrgui/Documents/GitHub/projetosPOO/identifier.sqlite";
+            String url = "jdbc:sqlite:C:/Users/mrgui/Documents/GitHub/projetosPOO/bancoTeste.db";
             Connection conn = DriverManager.getConnection(url);
             System.out.println("Conexão realizada com sucesso.");
 
@@ -20,18 +20,6 @@ class Main {
 
                 String insert2 = "INSERT INTO Clientes(nome_cliente) VALUES('Lúcio')";
                 statement2.execute(insert2);
-
-                String update1 = "UPDATE identifier SET nome_cliente = 'Lucas' WHERE id_cliente = 1";
-                statement1.execute(update1);
-
-                String update2 = "UPDATE identifier SET nome_cliente = 'Lucio' WHERE id_cliente = 2";
-                statement2.execute(update2);
-
-                String delete1 = "DELETE FROM identifier WHERE id = 1";
-                statement1.execute(delete1);
-
-                String delete2 = "DELETE FROM identifier WHERE id = 2";
-                statement2.execute(delete2);
 
             }catch (SQLException ex){
 
