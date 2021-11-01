c45bd53367ad72ce428139602ce2ed9a692606c9
guileraguiar <54964014+guileraguiar@users.noreply.github.com>
2021-10-31 22:07:22 -0300

    Atualização

--- jdbc/src/uni/uni/jdbc/ConexaoUtil.java
+++ jdbc/src/uni/uni/jdbc/ConexaoUtil.java
@@ -1,9 +1,5 @@
 package uni.jdbc;
-
-import com.mysql.cj.protocol.Resultset;
-
 import java.sql.*;
-
 
 /*
 
@@ -36,7 +32,7 @@ public class ConexaoUtil {
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
 
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection("jdbc:mysql://localhost:3306/bancoTeste","root","password");
 
     }
@@ -50,12 +46,12 @@ public class ConexaoUtil {
             Statement statement = conexaoUtil.getConnection().createStatement();
 
             ResultSet resultSet1 = statement.executeQuery("SELECT * FROM Clientes");
-
-            ResultSet resultSet2 = statement.executeUpdate("INSERT INTO Clientes (nome, sobrenome) VALUES ('João', 'Costa')");
-            ResultSet resultSet3 = statement.executeUpdate("INSERT INTO Clientes (nome, sobrenome) VALUES ('Maria', 'Clara')");
+            ResultSet resultSet2 = statement.executeUpdate("INSERT INTO Clientes (nome, sobrenome) VALUES ('Maria', 'Clara')");
+            ResultSet resultSet3 = statement.executeUpdate("UPDATE INTO Clientes SET nome = 'Marya' WHERE id_cliente = '1'");
+            ResultSet resultSet4 = statement.executeUpdate("DELETE FROM Clientes WHERE id_cliente = '1'");('Maria', 'Clara')");
 
             while (resultSet1.next()){
-                System.out.println(resultSet1.getString("nome"));
+                System.out.println(resultSet1);
             }
 
         }catch (Exception e) {
